95e581b0b618a382f676bf3de4703236e02e6a00
Tyler Ceja <ging@Tylers-MBP.attlocal.net>
2023-03-11 17:22:21 -0800

    writes data txt

--- soup.py
+++ soup.py
@@ -8,4 +8,13 @@ content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
 
-print(soup.prettify())
+box = soup.find('article', class_='main-article')
+#box is isolationg the main content of the page
+
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+#strip removes trailing and leading spaces only at the beginning and end of the data
+#separator replaces a new line with a blank space
+
+with open('budapest-noir.txt', 'w') as file:
+    file.write(transcript)
