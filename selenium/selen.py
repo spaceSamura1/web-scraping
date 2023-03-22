8a2aa6f3fb125a5ef51833c247338520b7c700e5
spaceSamura1 <tyler.ceja5@gmail.com>
2023-03-22 16:46:45 -0700

    adds click event

--- selenium/selen.py
+++ selenium/selen.py
@@ -1,13 +1,15 @@
 #Goal: How to find elements with Selenium
-
 from selenium import webdriver
+import time
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 path = '/Users/ging/Documents/coding/chromedriver_mac64/chromedriver'
 driver = webdriver.Chrome(path)
 driver.get(website)
 
+all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
+all_matches_button.click()
 
+time.sleep(10)
 
-
-driver.quit()
+# driver.quit()
